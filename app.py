a0facd45d5fe33c89ab795fb99feb64486dd08f1
Tarun Angalakuduru <91077827+TarunAngalakuduru@users.noreply.github.com>
2025-08-20 16:16:24 +0530

    modified the app python script

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 def main():
     print("Hello, World! This is my first simple Python script.")
+    print("This script is running on a Linux EC2 instance.")
 
 if __name__ == "__main__":
     main()
